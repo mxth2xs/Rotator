a4a8414af807187102de2973ae8bca2f8f815b72
mathias <mathiasdacsilva@gmail.com>
2022-06-01 18:49:59 +0200

    adding scribble

--- Algo/scribble.py
+++ Algo/scribble.py
@@ -1,10 +1,66 @@
 def scribble():
-    import tkinter
+    import tkinter as tk
+    from tkinter.colorchooser import askcolor
+
+    #Lancement de la fenêtre Canvas
+    scribble = tk.Tk()
+    C = tk.Canvas(scribble, bg="white", height=440+20, width=440)
+
+
+    def change_color():
+        global colors
+        colors = askcolor(title="Tkinter Color Chooser")
+        scribble.configure(bg=colors[1])
+
+
+    tk.Button(scribble, text='Select a Color', command=change_color).pack(expand=True)
+
+    def left_click(event):
+        global X,Y, colors, bandeau
+        X = event.x
+        Y = event.y
+        recup_coord()
+        if bandeau == True:
+            carre = C.create_rectangle(coord, fill = colors[1])
+    
+    def right_click(event):
+        global X,Y,colors, bandeau 
+        X = event.x
+        Y = event.y
+        colors = white
+        recup_coord()
+        if bandeau == True:
+            carre = C.create_rectangle(coord, fill = colors)
+        
+    scribble.bind('<Button-1>',left_click)
+    scribble.bind('<Button-2>',right_click)
+
+    def recup_coord():
+        global X,Y,coord, bandeau
+        if X//20 == 0 or Y//20 == 0:
+            if X//20 == 0 and Y//20 == 0:
+                X+=1
+                Y+=1
+            elif X//20 == 0:
+                X+=1
+            else:
+                Y+=1
+
+        if X <= 20:
+            bandeau = False
+        else:
+            bandeau = True
+
+        x1_final = (X//20)*20
+        y1_final = (Y//20)*20
+        x2_final = (X//20)*20+20
+        y2_final = (Y//20)*20+20
+        coord = x1_final, y1_final, x2_final, y2_final
+
+
+    #Affichage de la fenêtre Canvas
+    C.pack()
+    scribble.mainloop()
 
     
-
-    for x in range(10):
-        for y in range(10):
-            button = Button(Coche1,bd=10,width=7,height=3, background=couleur)
-            button['command']=couleurJ(button)
-            button.grid(row=y, column=x)
+scribble()
