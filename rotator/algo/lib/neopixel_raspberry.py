0b45a67fde6c1dedcfe01e61b3a7a21163e3ccb5
mathias <mathiasdacsilva@gmail.com>
2022-06-17 11:14:44 +0200

    Changement du nombre de led + test boucle d'import

--- rotator/algo/lib/neopixel_raspberry.py
+++ rotator/algo/lib/neopixel_raspberry.py
@@ -1,12 +1,13 @@
 import board
 import neopixel
+from config import num_led
 
 # Configure the setup
 PIXEL_PIN = board.D21  # pin that the NeoPixel is connected to
 ORDER = neopixel.GRB  # pixel color channel order
 
 # Create the NeoPixel object
-led = neopixel.NeoPixel(PIXEL_PIN, 1, pixel_order=ORDER)
+led = neopixel.NeoPixel(PIXEL_PIN, num_led, pixel_order=ORDER)
 
 class NeoPixel_raspberry(object):
     def setPixelColor(self, pixel, red, green, blue):
