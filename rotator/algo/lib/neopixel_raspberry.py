2a76f25b085a26931df1c8c1279d56aa09359389
mathias <mathiasdacsilva@gmail.com>
2022-06-17 11:05:43 +0200

    pixel conflict

--- rotator/algo/lib/neopixel_raspberry.py
+++ rotator/algo/lib/neopixel_raspberry.py
@@ -6,8 +6,8 @@ PIXEL_PIN = board.D21  # pin that the NeoPixel is connected to
 ORDER = neopixel.GRB  # pixel color channel order
 
 # Create the NeoPixel object
-pixel = neopixel.NeoPixel(PIXEL_PIN, 1, pixel_order=ORDER)
+led = neopixel.NeoPixel(PIXEL_PIN, 1, pixel_order=ORDER)
 
 class NeoPixel_raspberry(object):
     def setPixelColor(self, pixel, red, green, blue):
-        pixel[pixel] = (red, green, blue)
+        led[pixel] = (red, green, blue)
