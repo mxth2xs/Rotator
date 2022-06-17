696348eca6deda14ffe425c3acd5e8ced9b248b5
Mathias <mathiasdacsilva@gmail.com>
2022-06-17 22:05:01 +0200

    strand.show

--- rotator/algo/lib/neopixel_raspberry.py
+++ rotator/algo/lib/neopixel_raspberry.py
@@ -1,14 +1,14 @@
 import board
 import neopixel
 from config import num_led
-
+import rotator.algo.lib.neopixel_raspberry_pixelbuf as pixelbuf
 # Configure the setup
 PIXEL_PIN = board.D21  # pin that the NeoPixel is connected to
 ORDER = neopixel.GRB  # pixel color channel order
 
 # Create the NeoPixel object
-led = neopixel.NeoPixel(PIXEL_PIN, num_led, pixel_order=ORDER)
+led = neopixel.NeoPixel(PIXEL_PIN, num_led, brightness=0.2, auto_write=False, pixel_order=ORDER)
 
-class NeoPixel_raspberry(object):
+class NeoPixel_raspberry(pixelbuf.PixelBuf):
     def setPixelColor(self, pixel, red, green, blue):
         led[pixel] = (red, green, blue)
