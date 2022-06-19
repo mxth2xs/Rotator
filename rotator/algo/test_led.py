c6472b674e8a3f88cb1929703748226cd27ae18b
mathias <mathiasdacsilva@gmail.com>
2022-06-19 15:22:56 +0200

    Amélioration test_led

--- rotator/algo/test_led.py
+++ rotator/algo/test_led.py
@@ -2,23 +2,39 @@ def test_led():
     import time
     from rotator.config import strand, num_led, width, height
 
+    Time = 0.001
     while True:
+        for i in range (num_led-1):
+            strand.setPixelColor(i, 255, 0, 0)
+        
+        strand.show()
+        time.sleep(5)
+        strand.show()
+
         for i in range(num_led-1):
             strand.setPixelColor(i, 255, 0, 0)
-            time.sleep(0.001)
+            time.sleep(Time)
             strand.setPixelColor(i+1, 255, 0, 0)
-            time.sleep(0.001)
+            time.sleep(Time)
             strand.show()
             strand.setPixelColor(i, 0, 0, 0)
-            time.sleep(0.001)
+            time.sleep(Time)
             strand.setPixelColor(i+1, 0, 0, 0)
-            time.sleep(0.001)
+            time.sleep(Time)
             strand.show()
-            
+            if num_led == 484:
+                strand.setPixelColor(483, 255, 0, 0)
+                strand.setPixelColor(482, 255, 0, 0)
+                time.sleep(3)
+                strand.show()
+            elif num_led == 968:
+                strand.setPixelColor(483, 255, 0, 0)
+                strand.setPixelColor(482, 255, 0, 0)
+                strand.setPixelColor(967, 255, 0, 0)
+                strand.setPixelColor(466, 255, 0, 0)
+                time.sleep(5)
+                strand.show()
 
-    """while True:
-        strand.setPixelColor(483, 255, 0, 0)
-        strand.setPixelColor(482, 255, 0, 0)"""
 
 if __name__ == "__main__":
     test_led()
